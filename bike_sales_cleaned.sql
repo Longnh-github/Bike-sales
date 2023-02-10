32999bbb592b81495dd467a9195a9d064f1e3478
Long Nguyen <124225372+Longnh-github@users.noreply.github.com>
2023-02-10 16:07:16 +0900

    Add files via upload

---
+++ bike_sales_cleaned.sql
@@ -0,0 +1,38 @@
+SELECT 
+	ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name) AS customer_name,
+	cus.city,
+	cus.state,
+	order_date,
+	SUM(ite.quantity) AS total_unit,
+	SUM(ite.quantity * ite.list_price) AS revenue,
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name) AS sale_rep
+FROM sales.orders AS ord
+JOIN sales.customers AS cus
+ON ord.customer_id = cus.customer_id
+JOIN sales.order_items as ite
+ON ord.order_id = ite.order_id
+JOIN production.products AS pro
+ON pro.product_id = ite.product_id
+JOIN production.categories AS cat
+ON cat.category_id = pro.category_id
+JOIN sales.stores AS sto
+ON sto.store_id = ord.store_id
+JOIN sales.staffs AS sta
+ON sta.staff_id = ord.staff_id
+GROUP BY 
+	ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name),
+	cus.city,
+	cus.state,
+	order_date,
+	product_name,
+	category_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name)
+
+ORDER BY 
+	ord.order_id
